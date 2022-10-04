a7190c3391fa6c1242e19e205f6b4d6c6ab45d43
Bradley <bradsamsess90@icloud.com>
2022-10-04 15:17:05 -0400

    first commit

--- assets/db/schema.sql
+++ assets/db/schema.sql
@@ -12,7 +12,7 @@ CREATE TABLE roles(
     id  INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT
+    department_id INT,
     FOREIGN KEY (department_id)
     REFERENCES department(id)
 );
@@ -22,7 +22,7 @@ CREATE TABLE  empolyees(
     first_name VARCHAR(30) NOT NULL,
     last_name  VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id  INT
+    manager_id  INT,
     FOREIGN KEY (role_id)
     REFERENCES roles(id)
 );
